f7999eca6f10d078880980f0bd3a65204e965dcb
Nitesh Kawale <niteshkawale123.nk@gmail.com>
2021-01-21 17:36:15 +0530

    Hi nk

---
+++ if_else.py
@@ -0,0 +1,13 @@
+# number guessing Game
+guessing_no = 25
+winning_no = 24
+if winning_no == guessing_no:
+    print("YOU are THE WINNER")
+else:
+    if winning_no > guessing_no:
+        print("to LOW")
+    else: 
+        winning_no < guessing_no 
+        print("TO High")
+    
+    
